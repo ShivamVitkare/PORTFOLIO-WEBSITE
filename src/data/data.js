b00ff8ea0077ef3b97d2179c5e67647b07a54717
Shivam Vitkare <shivamvitkare25@gmail.com>
2023-06-14 21:00:28 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+
+
+export const data=[
+    {
+        id:1,
+        name:"E-commerce App",
+        image:"https://shipsy.io/wp-content/uploads/2021/02/Blog-91.jpg",
+        github:"https://github.com/ShivamVitkare/shopping-cart-feature",
+        live:"https://e-commerce-shivam.netlify.app",
+    },
+    {
+        id:2,
+        name:"Food App",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdy5Al2JGTYrKHEpddus-OiwUcdPbIwNuilw&usqp=CAU",
+        github:"https://github.com/ShivamVitkare/FOOD_ORDERING_APP",
+        live:"https://shivam-food-website.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Weather-App",
+        image:"https://www.the-sun.com/wp-content/uploads/sites/6/2022/04/JF-US-STAY-DRY-WEATHER-APP-COMP.jpg",
+        github:"https://github.com/ShivamVitkare/Weather-App",
+        live:"https://shivamvitkare.github.io/Weather-App/",
+    },
+    {
+        id:4,
+        name:"Crypto-Converter-App",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSh9oB08rlOlDRm29Q-g44xFTHwV0rGzFl1cA&usqp=CAU",
+        github:"https://github.com/ShivamVitkare/Crypto-converter-app",
+        live:"https://shivam-crypto-converter.netlify.app",
+    },
+    {
+        id:5,
+        name:"Quiz-App",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvyqp-lrwIvdGDuidfQfCAP6SlTuOTogbHbg&usqp=CAU",
+        github:"https://github.com/ShivamVitkare/Quiz-app",
+        live:"https://shivam-quiz-app.netlify.app/",
+    },
+    {
+        id:5,
+        name:"Mi-Store-clone",
+        image:"https://www.shutterstock.com/image-photo/taipei-taiwan-december-4-2018-260nw-1620871072.jpg",
+        github:"https://github.com/ShivamVitkare/Mi-store-clone",
+        live:"https://shivam-mi-clone.netlify.app/",
+    },
+
+
+]
